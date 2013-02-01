e49ce5275a7036a2a940d73074b6e477caea36a0
Will Mayner <wmayner@gmail.com>
2013-02-01 14:06:25 -0500

    new GA

--- public/js/googleAnalytics.js
+++ public/js/googleAnalytics.js
@@ -1,5 +1,6 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-35736272-1']);
+_gaq.push(['_setAccount', 'UA-35736272-3']);
+_gaq.push(['_setDomainName', 'willmayner.com']);
 _gaq.push(['_trackPageview']);
 
 (function() {
